1717030dc1d4a207e36f790a7e376f518c867ddd
gustavo <gustavo.isaac@gmail.com>
2023-01-17 07:56:02 +0000

    RepositoryModule

---
+++ app/src/main/java/com/artemissoftware/stockmarket/di/RepositoryModule.kt
@@ -0,0 +1,30 @@
+package com.artemissoftware.stockmarket.di
+
+
+import com.artemissoftware.stockmarket.data.csv.CSVParser
+import com.artemissoftware.stockmarket.data.csv.CompanyListingsParser
+import com.artemissoftware.stockmarket.data.repository.StockRepositoryImpl
+import com.artemissoftware.stockmarket.domain.models.CompanyListing
+import com.artemissoftware.stockmarket.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
