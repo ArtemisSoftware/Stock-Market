837f5653f0d5a5189386ef939df684f340c00890
gustavo <gustavo.isaac@gmail.com>
2023-01-18 08:15:51 +0000

    IntradayInfoMapper

---
+++ app/src/main/java/com/artemissoftware/stockmarket/data/mapper/IntradayInfoMapper.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.stockmarket.data.mapper
+
+import com.artemissoftware.stockmarket.data.remote.dto.IntradayInfoDto
+import com.artemissoftware.stockmarket.domain.models.IntradayInfo
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+fun IntradayInfoDto.toIntradayInfo(): IntradayInfo {
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
+    val localDateTime = LocalDateTime.parse(timestamp, formatter)
+    return IntradayInfo(
+        date = localDateTime,
+        close = close
+    )
+}
