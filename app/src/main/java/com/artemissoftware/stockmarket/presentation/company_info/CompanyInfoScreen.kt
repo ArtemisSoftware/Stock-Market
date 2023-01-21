81ae403ee6dfe9fa8f84edbba314ece85c2cc7db
gustavo <gustavo.isaac@gmail.com>
2023-01-21 13:48:33 +0000

    Height fix

--- app/src/main/java/com/artemissoftware/stockmarket/presentation/company_info/CompanyInfoScreen.kt
+++ app/src/main/java/com/artemissoftware/stockmarket/presentation/company_info/CompanyInfoScreen.kt
@@ -81,7 +81,7 @@ fun CompanyInfoScreen(
                 if(state.stockInfos.isNotEmpty()) {
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(text = "Market Summary")
-                    Spacer(modifier = Modifier.height(32.dp))
+                    Spacer(modifier = Modifier.height(80.dp))
                     StockChart(
                         infos = state.stockInfos,
                         modifier = Modifier
